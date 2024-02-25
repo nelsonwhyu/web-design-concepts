e4eb553ad8fbeb39c84165b8bcaaa5a1c6988bba
Nelson Yu <nelsonwhyu@gmail.com>
2024-02-24 20:13:29 -0500

    feat: add Vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
-  import '../app.css'
-  import Header from '../components/header/header.svelte';
+	import '../app.css';
+	import Header from '../components/header/header.svelte';
+
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectSpeedInsights();
 </script>
 
 <Header class="sticky top-0 z-50 text-primary backdrop-blur" />
